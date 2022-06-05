423619cd96e4497f7d93769b50dfec6fcf953f74
Tartessus <carloslbm@hotmail.com>
2022-06-05 13:23:21 +0200

    arreglo prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   logo: '../assets/logo.jpg',
   urlProyecto: 'https://git.institutomilitar.com/Tartessus/SERVICASA',
-  host: 'https://servicasaapp.herokuapp.com/',
+  host: 'https://servicasaapp.herokuapp.com/api/',
   itemsPorPagina: 10
 };
